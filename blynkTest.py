1cf9ec03e15e4f2c030066fca4739e09e58cfd8f
HappySpyH107 <48614061+HappySpyH107@users.noreply.github.com>
2020-08-17 11:01:10 +0800

    Update blynkTest.py

--- blynkTest.py
+++ blynkTest.py
@@ -13,6 +13,8 @@ import json
 import math
 import Adafruit_DHT
 
+x =0
+
 camera = PiCamera()
 
 BLYNK_AUTH = 'Rtyir7iUZ0pA3IeQIR-zOyEH42QhuE-x'
@@ -56,65 +58,84 @@ while True:
 
     if crowd == 0:
         status = "Empty"
+        blynk.virtual_write(1, " ")
+        x =0
+        blynk.virtual_write(5, 1, '', '', "value")
+
     elif crowd >= 1:
         status = "Low crowd"
 
-        if __name__ == "__main__":
-            # Authentication parameters
-            headers = {'AccountKey': 'NXpFdS3XQnqaH4jIc2g6kQ==',
-                       'accept': 'application/json'}  # this is by default
+        if x == 0:
+            if __name__ == "__main__":
+                # Authentication parameters
+                headers = {'AccountKey': 'NXpFdS3XQnqaH4jIc2g6kQ==',
+                           'accept': 'application/json'}  # this is by default
 
-            # API parameters
-            uri = 'http://datamall2.mytransport.sg/'  # Resource URL
-            path = 'ltaodataservice/BusArrivalv2?BusStopCode=75279'
+                # API parameters
+                uri = 'http://datamall2.mytransport.sg/'  # Resource URL
+                path = 'ltaodataservice/BusArrivalv2?BusStopCode=75279'
 
-        # Build query string & specify type of API call
-        target = urlparse(uri + path)
-        method = 'GET'
-        body = ''
+                # Build query string & specify type of API call
+            target = urlparse(uri + path)
+            method = 'GET'
+            body = ''
 
-        # Get handle to http
-        h = http.Http()
+            # Get handle to http
+            h = http.Http()
 
-        # Obtain results
-        response, content = h.request(target.geturl(), method, body, headers)
+            # Obtain results
+            response, content = h.request(target.geturl(), method, body, headers)
 
-        # Parse JSON to print
-        jsonObj = json.loads(content)
+            # Parse JSON to print
+            jsonObj = json.loads(content)
 
-        z = len(jsonObj['Services'])
+            z = len(jsonObj['Services'])
 
-        BusService = ['', '', '', '', '']
-        firstBus = ['', '', '', '', '']
-        Bus1 = ['', '', '', '', '']
-        BusTiming1 = ['', '', '', '', '']
-        BusArr1 = ['', '', '', '', '']
-        min1 = ['', '', '', '', '']
+            BusService = ['', '', '', '', '']
+            firstBus = ['', '', '', '', '']
+            Bus1 = ['', '', '', '', '']
+            BusTiming1 = ['', '', '', '', '']
+            BusArr1 = ['', '', '', '', '']
+            min1 = ['', '', '', '', '']
+            lat = ['', '', '', '', '']
+            long = ['', '', '', '', '']
 
-        for x in range(z):
-            BusService[x] = jsonObj['Services'][x]['ServiceNo']
-            firstBus[x] = jsonObj['Services'][x]['NextBus']['EstimatedArrival']
+            for x in range(z):
+                BusService[x] = jsonObj['Services'][x]['ServiceNo']
+                firstBus[x] = jsonObj['Services'][x]['NextBus']['EstimatedArrival']
+                lat[x] = jsonObj['Services'][x]['NextBus']['Latitude']
+                long[x] = jsonObj['Services'][x]['NextBus']['Longitude']
 
-            Bus1[x] = (firstBus[x].replace("T", " ")).replace("+08:00", "")
-            BusTiming1[x] = datetime.datetime.strptime(Bus1[x], '%Y-%m-%d %H:%M:%S')
-            BusArr1[x] = ((BusTiming1[x] - now).total_seconds()) / 60
-            min1[x] = math.trunc(BusArr1[x])
+                Bus1[x] = (firstBus[x].replace("T", " ")).replace("+08:00", "")
+                BusTiming1[x] = datetime.datetime.strptime(Bus1[x], '%Y-%m-%d %H:%M:%S')
+                BusArr1[x] = ((BusTiming1[x] - now).total_seconds()) / 60
+                min1[x] = math.trunc(BusArr1[x])
 
-            print(BusService[x])
-            print(min1[x])
-            print("\n")
-
-        for x in range(z):
-            if min1[x] > 10:
                 print(BusService[x])
-                blynk.notify("High Crowd! Please deploy additional bus  "+BusService[x])
-                blynk.virtual_write(1, BusService[x])
+                print(min1[x])
+                print("\n")
 
-        blynk.notify("High Crowd, no additional deployment of buses are required")
-        blynk.run()
+                blynk.virtual_write(1, " ")
+
+            for x in range(z):
+                if min1[x] > 10:
+                    print(BusService[x])
+                    blynk.notify("High Crowd! Please deploy additional bus  " + BusService[x])
+                    blynk.virtual_write(1, BusService[x])
+                    blynk.virtual_write(5, 1, lat[x], long[x], "value")
+
+            blynk.notify("High Crowd, no additional deployment of buses are required")
+            blynk.run()
+
+            x = 1
+
+
 
     elif crowd > 15:
         status = "High crowd"
+        blynk.virtual_write(1, " ")
+        x =0
+        blynk.virtual_write(3, 1, '', '', "value")
 
     print(status)
 
@@ -123,3 +144,4 @@ while True:
     blynk.run()
 
     weather()
+
